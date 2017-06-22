0d518e79f55812d3059ce0a0e1c89eb2ded44fcd
최솔지 <kairess89@naver.com>
2017-06-22 18:02:45 +0900

    dd

---
+++ inc/footer.php
@@ -0,0 +1,74 @@
+            <div id="main_footer">
+                <ul class="sitemap">
+                    <li>
+                        <dl>
+                            <dt><a href="#">Regions</a></dt>
+                            <dd><a href="#">U.S.</a></dd>
+                            <dd><a href="#">Africa</a></dd>
+                            <dd><a href="#">Americas</a></dd>
+                            <dd><a href="#">Asia</a></dd>
+                            <dd><a href="#">China</a></dd>
+                            <dd><a href="#">Europe</a></dd>
+                            <dd><a href="#">Middle East</a></dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt><a href="#">U.S. Politics</a></dt>
+                            <dd><a href="#">45</a></dd>
+                            <dd><a href="#">Congress</a></dd>
+                            <dd><a href="#">Security</a></dd>
+                            <dd><a href="#">The Nine</a></dd>
+                            <dd><a href="#">Trumpmerica</a></dd>
+                            <dd><a href="#">State</a></dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt><a href="#">Money</a></dt>
+                            <dd><a href="#">Business</a></dd>
+                            <dd><a href="#">Markets</a></dd>
+                            <dd><a href="#">Tech</a></dd>
+                            <dd><a href="#">Luxury</a></dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt><a href="#">entertainment</a></dt>
+                            <dd><a href="#">Stars</a></dd>
+                            <dd><a href="#">Screen</a></dd>
+                            <dd><a href="#">Binge</a></dd>
+                            <dd><a href="#">Culture</a></dd>
+                            <dd><a href="#">Media</a></dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt><a href="#">tech</a></dt>
+                            <dd><a href="#">Business</a></dd>
+                            <dd><a href="#">Culture</a></dd>
+                            <dd><a href="#">Gadgets</a></dd>
+                            <dd><a href="#">Future</a></dd>
+                            <dd><a href="#">Startups</a></dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt><a href="#">Sport</a></dt>
+                            <dd><a href="#">Football</a></dd>
+                            <dd><a href="#">Golf</a></dd>
+                            <dd><a href="#">Tennis</a></dd>
+                            <dd><a href="#">Motorsport</a></dd>
+                            <dd><a href="#">Horseracing</a></dd>
+                            <dd><a href="#">Equestrian</a></dd>
+                            <dd><a href="#">Sailing</a></dd>
+                        </dl>
+                    </li>
+                </ul>
+
+                <p class="copyright">&copy; 2017 Cable News Network. Turner Broadcasting System, Inc. All Rights Reserved.</p>
+            </div><!-- /main_footer-->
+        </div>
+        <script type="text/javascript" scr="//cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.4/build/jquery.datetimepicker.full.min.js"></script>
+    </body>
+</html>
