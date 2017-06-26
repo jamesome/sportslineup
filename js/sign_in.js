c90a5b6d93bdab7fb1d0e331261f09d2fd182ff3
김영민 <jamesome@naver.com>
2017-06-26 11:20:34 +0900

    로그인 js

--- js/sign_in.js
+++ js/sign_in.js
@@ -8,18 +8,42 @@
         login: function() {
             var login_id = $('#login_id').val();
             var login_pw = $('#login_pw').val();
+            var retn = true;
 
-            console.log(login_id, login_pw)
-        },
-        a: function() {
-            console.log(1);
+            if (retn && login_id == '') {
+                common.commmon_alert('Please Enter your ID', '', 'error');
+                retn = false;
+            }
+            if (retn && login_pw == '') {
+                common.commmon_alert('Please Enter your PW', '', 'error');
+                retn = false;
+            }
+
+            if (!retn) {
+                $("#login").on('click', sign_in.login);
+                return false;
+            }
+
+            $.post("/ajax/ajax_set_user_login.php", {
+                    'userid': login_id,
+                    'userpw': login_pw
+                },
+                function(data) {
+                    if (data == 'ok') {
+                        common.commmon_alert('Signed in', '', 'success');
+                    } else if (data == 'uid') {
+                        common.commmon_alert('Please Check your ID', '', 'info');
+                    } else if (data == 'upass') {
+                        common.commmon_alert('Please Check your PW', '', 'info');
+                    }
+                });
         }
+
     };
 })(window);
 
 $(function() {
-    $('#sign_up').click(function() {
-        alert('a');
+    $('#login').click(function() {
+        sign_in.login();
     });
-    sign_in.a();
 });
